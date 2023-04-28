db0b7df4c72c832a18ccce27ecd0ad79b36acde4
KimEunA97 <kimeuna9712@gmail.com>
2023-04-28 17:31:14 +0900

    72쪽

---
+++ components/Greeting.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { view, Text } from 'react-native'
+
+function Greeting() {
+  return (
+    <view>
+      <Text>안녕하세요 함수 컴포넌트</Text>
+    </view>
+    )}
+
+    export default Greeting;
